5ed3bf3c544ddaacd6f10772abe0ef82774b99a0
aashxs <aashxsh@gmail.com>
2024-09-24 20:55:17 +0530

    intialCommit

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,20 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const [projects, setProjects] = useState([
+    { name: 'Project 1', description: 'This is project 1' },
+    { name: 'Project 2', description: 'This is project 2' },
+    // Add more projects as you see fit
+  ]);
   return (
-    <div id="main"></div>
+    <div className="App">
+    {projects.map((project, index) => (
+      <div key={index}>
+        <h1 data-ns-test="project-name">{project.name}</h1>
+        <p data-ns-test="project-description">{project.description}</p>
+      </div>
+    ))}
+  </div>
   )
 }
 
